1ba731cf00240d4ff8a6523196a691552237b08f
Omar Zhadykov <omar3282088@gmail.com>
2025-12-06 11:05:37 +0100

    added models package into nest and angular

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MY_APP_NAME } from '@life-os/models';
 
 @Controller()
 export class AppController {
---
+++ packages/models/src/index.ts
@@ -0,0 +1,1 @@
+export const MY_APP_NAME = 'Life OS v1';
